3ff52c04a7bc787c57ea037763cde4b9d64069c6
Eduardo Grajeda <tatofoo@gmail.com>
2018-11-23 17:29:34 -0300

    Add base todo-edge-service.

---
+++ todo-edge-service/src/main/java/com/egrajeda/todoedgeservice/TodoEdgeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.egrajeda.todoedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoEdgeServiceApplication.class, args);
+	}
+}
---
+++ todo-edge-service/src/test/java/com/egrajeda/todoedgeservice/TodoEdgeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.egrajeda.todoedgeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoEdgeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
