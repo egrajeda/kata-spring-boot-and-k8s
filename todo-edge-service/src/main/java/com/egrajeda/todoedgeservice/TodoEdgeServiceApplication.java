7308e332f1cf4316af41713fb1e914c42fd251ea
Eduardo Grajeda <tatofoo@gmail.com>
2018-11-23 17:44:57 -0300

    Add a @FeignClient to todo-edge-service.

--- todo-edge-service/src/main/java/com/egrajeda/todoedgeservice/TodoEdgeServiceApplication.java
+++ todo-edge-service/src/main/java/com/egrajeda/todoedgeservice/TodoEdgeServiceApplication.java
@@ -1,12 +1,50 @@
 package com.egrajeda.todoedgeservice;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
+@EnableFeignClients
 @SpringBootApplication
 public class TodoEdgeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoEdgeServiceApplication.class, args);
+	}
+}
+
+@Data
+class Todo {
+
+	private String title;
+}
+
+@FeignClient("todo-service")
+interface TodoClient {
+
+	@GetMapping("/todos")
+	Resources<Todo> readTodos();
+}
+
+@RestController
+class TodoApiAdapterRestController {
+
+	private final TodoClient todoClient;
+
+	TodoApiAdapterRestController(TodoClient todoClient) {
+		this.todoClient = todoClient;
+	}
+
+	@GetMapping("/todos")
+	public Collection<Todo> todos() {
+		return todoClient.readTodos()
+				.getContent();
 	}
 }
